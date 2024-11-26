b5eb2d370f7dc831933ffe09b06153550b470a8a
Kobeng Chantel <kobengchantel@gmail.com>
2024-11-26 11:12:16 +0200

    weatherApp

---
+++ src/app/atom.ts
@@ -0,0 +1,10 @@
+/** @format */
+
+import { atom } from 'jotai';
+
+export const placeAtom = atom ("Republic of India");
+
+// import { atom } from 'jotai';
+
+export const loadingCityAtom = atom(false);
+// export const placeAtom = atom("");
